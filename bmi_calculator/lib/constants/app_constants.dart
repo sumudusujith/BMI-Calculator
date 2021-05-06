91f5d71c095d58373fda48c5df01c803db992f33
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-07 00:31:29 +0530

    blue cl

--- bmi_calculator/lib/constants/app_constants.dart
+++ bmi_calculator/lib/constants/app_constants.dart
@@ -3,4 +3,4 @@ import 'package:flutter/cupertino.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 final Color mainHexColor = HexColor("#1C1C1C");
-final Color accentHexColor = HexColor("#FCC91C");
+final Color accentHexColor = HexColor("#1C9BFC");
